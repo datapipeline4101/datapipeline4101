78834e971e343f0a03bde02b0942a6de21e25435
William Stern <sternwill970@gmail.com>
2023-04-16 16:41:48 -0400

    add

--- code/milestone4_Mac.py
+++ code/milestone4_Mac.py
@@ -8,33 +8,12 @@ import math
 
 
 def import_package_windows(package):
-    # import subprocess
-    # import sys
-    # import os
-    
-    # # path to python.exe
-    # python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
-    
-    # # upgrade pip
-    # subprocess.call([python_exe, "-m", "ensurepip"])
-    # subprocess.call([python_exe, "-m", "pip", "install", "--upgrade", "pip"])
-    
-    # # install required packages
-    # subprocess.call([python_exe, "-m", "pip", "install", package])
 
     import importlib
-
-    #i = importlib.import_module(package)
 
     import subprocess
     import ensurepip
     import sys
-    # ensurepip.bootstrap()
-    # pybin = sys.executable
-    # subprocess.check_call([pybin, '-m', 'pip', 'install', package])
-    #import importlib
-
-    # 
 
     
     python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
@@ -80,8 +59,6 @@ def back_ground_adder(filepath):
     # Scales image to dimensions set in the Render panel, my case 1280x720
     scale_node.space = "RENDER_SIZE"
 
-    # Select output folder, i.e. where to store rendered images
-    #file_output_node.base_path = "C:/tmp/"
 
     # Scale background image
     links.new(image_node.outputs[0], scale_node.inputs[0])
@@ -120,49 +97,7 @@ def find_xy(p1, p2, z):
     return x, y
 
 
-def back_ground_adder(filepath):
 
-    bpy.context.scene.use_nodes = True
-    tree = bpy.context.scene.node_tree
-    links = tree.links
-
-    for node in tree.nodes:
-        tree.nodes.remove(node)
-
-    image_node = tree.nodes.new('CompositorNodeImage')
-    scale_node = tree.nodes.new('CompositorNodeScale')
-    alpha_over_node = tree.nodes.new('CompositorNodeAlphaOver')
-    render_layer_node = tree.nodes.new('CompositorNodeRLayers')
-    output_node = tree.nodes.new('CompositorNodeComposite')
-
-    # Scales image to dimensions set in the Render panel, my case 1280x720
-    scale_node.space = "RENDER_SIZE"
-
-    # Select output folder, i.e. where to store rendered images
-    #file_output_node.base_path = "C:/tmp/"
-
-    # Scale background image
-    links.new(image_node.outputs[0], scale_node.inputs[0])
-
-    # Set background image as background image input to alpha node
-    links.new(scale_node.outputs[0], alpha_over_node.inputs[1]) #1
-
-    # Set rendered object as the foreground image to alpha node
-    links.new(render_layer_node.outputs[0], alpha_over_node.inputs[2]) #2
-
-   
-    # Set rendered object as the foreground image to alpha node
-    links.new(render_layer_node.outputs[0], alpha_over_node.inputs[2]) #2
-
-    # Final image is the output image
-    links.new(alpha_over_node.outputs[0], output_node.inputs[0])
-    bpy.context.scene.render.film_transparent = True
-    
-    
-    # Load background image and set output file
-    image_node = bpy.context.scene.node_tree.nodes[0]
-    image_node.image = bpy.data.images.load(filepath)
-    #file_output_node.file_slots[0].path = 'blender-######.color.png' # blender placeholder #
 
 
 def create_camera(location, rotation, lens):
@@ -176,13 +111,18 @@ def create_camera(location, rotation, lens):
     scene.collection.objects.link(cam_obj1)
     bpy.context.scene.camera = bpy.data.objects["Camera 1"]
 
-def create_light(type, energy, location):
+def create_light(type, energy, location, rotation=[1.5, 0.0, 0.0]):
+
+    
     # Create light datablock
     light_data = bpy.data.lights.new(name="my-light-data", type=type)
     light_data.energy = energy
 
     # Create new object, pass the light data 
     light_object = bpy.data.objects.new(name="my-light", object_data=light_data)
+
+    if (type=="SUN"):
+        light_object.rotation_euler = rotation
 
     # Link object to collection in context
     bpy.context.collection.objects.link(light_object)
@@ -218,55 +158,24 @@ def get_location(obj, frame):
 
     return(global_location[0], global_location[1], global_location[2], global_location[3])
 
-def main():
-    # bpy.ops.wm.read_homefile(use_empty=True)
-    delete_all_objects()
-    
-    base_path = os.getcwd() 
-    import_package_windows("toml")
-    import toml
-    import_package_windows("tqdm")
-    from tqdm import tqdm
-    # import_package_windows("scipy")
-    # "\tomconfig.toml" changed to "/tomconfig.toml"
-    toml_path = base_path + r"/code/tomconfig_mac.toml"
-    print(base_path)
-
-    with open(toml_path, "rb") as f:
-        toml_dict = toml.load(toml_path)
-    
-
-    sat_path = (base_path + toml_dict["satellite"]["satellite_file"]).encode('unicode_escape')
-    bpy.ops.import_scene.obj(filepath=sat_path)
-
-    sat = bpy.context.selected_objects
-
-    earth_path = (base_path + toml_dict["earth"]["earth_file"]).encode('unicode_escape')
-    bpy.ops.import_scene.obj(filepath=earth_path)
-
-    earth = bpy.context.selected_objects
-    print(earth)
-    for e in earth:
-        e.location = [25, 0, 0]
-    #delete_all_objects()
-
-    print(sat)
+def create_stars(star_config):
     import random
+    from tqdm import tqdm
 
     # Set the number of stars to create
-    num_stars = 1000
+    num_stars = star_config["num_stars"]
 
     # Set the size range for the stars
-    min_size = 0.05
-    max_size = 0.2
+    min_size = star_config["min_size"]
+    max_size = star_config["max_size"]
 
     # Set the location range for the stars
-    min_loc = -300
-    max_loc = 300
-
+    min_loc = star_config["min_dist"]
+    max_loc = star_config["max_dist"]
+    random.seed(star_config["star_seed"])
     # Set the brightness range for the stars
-    min_bright = 100
-    max_bright = 600
+    min_bright = star_config["min_bright"]
+    max_bright = star_config["max_bright"]
 
     # Create a new material for the stars
     mat = bpy.data.materials.new(name="StarMaterial")
@@ -276,7 +185,7 @@ def main():
     emission_node.inputs[0].default_value = (1.0, 1.0, 1.0, 1.0)
 
     # Create the stars
-    for i in tqdm(range(num_stars)):
+    for i in tqdm(range(int(num_stars))):
         # Create a new sphere mesh for the star
         bpy.ops.mesh.primitive_uv_sphere_add(radius=random.uniform(min_size, max_size), location=(random.uniform(min_loc, max_loc), random.uniform(min_loc, max_loc), random.uniform(min_loc, max_loc)))
 
@@ -299,7 +208,40 @@ def main():
         bpy.ops.transform.translate(value=(random.uniform(min_loc, max_loc), random.uniform(min_loc, max_loc), random.uniform(min_loc, max_loc)))
 
 
+def main():
+    # bpy.ops.wm.read_homefile(use_empty=True)
+    delete_all_objects()
+    
+    base_path = os.getcwd() 
+    import_package_windows("toml")
+    import toml
+    import_package_windows("tqdm")
+    from tqdm import tqdm
+    toml_path = base_path + r"/code/tomconfig_mac.toml"
+    print(base_path)
 
+    with open(toml_path, "rb") as f:
+        toml_dict = toml.load(toml_path)
+    
+
+    sat_path = (base_path + toml_dict["satellite"]["satellite_file"]).encode('unicode_escape')
+    bpy.ops.import_scene.obj(filepath=sat_path)
+
+    sat = bpy.context.selected_objects
+
+    earth_path = (base_path + toml_dict["earth"]["earth_file"]).encode('unicode_escape')
+    bpy.ops.import_scene.obj(filepath=earth_path)
+
+    earth = bpy.context.selected_objects
+    print(earth)
+    for e in earth:
+        e.location = toml_dict["earth"]["earth_location"]
+    #delete_all_objects()
+
+    print(sat)
+    
+
+    create_stars(toml_dict["stars"])
 
     world = bpy.data.worlds["World"]
     world.node_tree.nodes["Background"].inputs[0].default_value = (0, 0, 0, 1)
@@ -310,8 +252,12 @@ def main():
 
     camera_settings = toml_dict["camera"]
     create_camera(location=camera_settings["location"], rotation=camera_settings["rotation"], lens=camera_settings["lens"])
-    #back_ground_adder((base_path+toml_dict["background"]["background_file"]))
+    if toml_dict["background"]["static_background"]:
+        back_ground_adder((base_path+toml_dict["background"]["background_file"]))
     scene = bpy.context.scene
+    fps = toml_dict["animation"]["fps"]
+    scene.render.fps = fps
+    
     
     positions = toml_dict["flightpath"]["positions"]
     rotations = toml_dict["flightpath"]["rotations"]
@@ -321,7 +267,7 @@ def main():
 
     len_of_ani = ani_setting["animation_lenth"]
 
-    frame_gap = (len_of_ani*24) // (len(positions)-1)
+    frame_gap = (len_of_ani*fps) // (len(positions)-1)
 
     print("FRAMEGAP:", frame_gap)
 
@@ -344,17 +290,20 @@ def main():
             number_of_frame += frame_gap-1
         #print("FFFF")
     cam = bpy.data.objects['Camera 1']
-    bpy.ops.object.empty_add(location=(0, 0, 0))
-    constraint = cam.constraints.new(type='TRACK_TO')
-    constraint.target = bpy.data.objects['Empty']
-    constraint.track_axis = 'TRACK_NEGATIVE_Z'
-    constraint.up_axis = 'UP_Y'
+
+    # This sets an invisible object that the camera will always point to if view_locked = True
+    bpy.ops.object.empty_add(location=tuple(toml_dict["camera"]["lock_point"]))
+    if toml_dict["camera"]["view_locked"]:
+        constraint = cam.constraints.new(type='TRACK_TO')
+        constraint.target = bpy.data.objects['Empty']
+        constraint.track_axis = 'TRACK_NEGATIVE_Z'
+        constraint.up_axis = 'UP_Y'
 
     if toml_dict["camera_flightpath"]["flight_path_type"] == "POINT":
         camera_positions = toml_dict["camera_flightpath"]["positions"]
         camera_rotations = toml_dict["camera_flightpath"]["rotations"]
         
-        camera_frame_gap = (len_of_ani*24) // (len(positions)-1)
+        camera_frame_gap = (len_of_ani*fps) // (len(positions)-1)
         number_of_frame = 0  
         for idx in tqdm(range(len(positions))):
             
@@ -378,9 +327,8 @@ def main():
         camera_positions = toml_dict["camera_flightpath"]["positions"]
         camera_rotations = toml_dict["camera_flightpath"]["rotations"]
         
-        camera_frame_gap = (len_of_ani*24) // (len(positions)-1)
+        camera_frame_gap = (len_of_ani*fps) // (len(positions)-1)
         number_of_frame = 0 
-        print("AAAAAAAAAAAAAAAAAAAAAAAA")
         # def flight_path(x, frame_distance):
         #     new_position_x = x + frame_distance[0] #pow function isn't working
         #     new_position_y = eval(toml_dict["flightpath"]["y_eq"])
@@ -404,7 +352,7 @@ def main():
         # end_position = toml_dict["flightpath"]["positions"][1]
         # start_to_finish = tuple(map(lambda i, j: j-i, start_position, end_position))
         
-        position_amount = toml_dict["animation"]["animation_lenth"] * 24
+        position_amount = toml_dict["animation"]["animation_lenth"] * fps
         # frame_distance = tuple(map(lambda i: i/position_amount, start_to_finish))
 
 
@@ -423,7 +371,7 @@ def main():
 
         len_of_ani = ani_setting["animation_lenth"]
 
-        frame_gap = (len_of_ani*24) // (len(positions)-1)
+        frame_gap = (len_of_ani*fps) // (len(positions)-1)
 
 
         # start with frame 0
@@ -454,7 +402,6 @@ def main():
 
     if toml_dict["flightpath"]["flight_path_type"] == "FUNCTION":
         positions = []
-        print("AAAAAAAAAAAAAAAAAAAAAAAA")
         # def flight_path(x, frame_distance):
         #     new_position_x = x + frame_distance[0] #pow function isn't working
         #     new_position_y = eval(toml_dict["flightpath"]["y_eq"])
@@ -478,7 +425,7 @@ def main():
         # end_position = toml_dict["flightpath"]["positions"][1]
         # start_to_finish = tuple(map(lambda i, j: j-i, start_position, end_position))
         
-        position_amount = toml_dict["animation"]["animation_lenth"] * 24
+        position_amount = toml_dict["animation"]["animation_lenth"] * fps
         # frame_distance = tuple(map(lambda i: i/position_amount, start_to_finish))
 
 
@@ -497,7 +444,7 @@ def main():
 
         len_of_ani = ani_setting["animation_lenth"]
 
-        frame_gap = (len_of_ani*24) // (len(positions)-1)
+        frame_gap = (len_of_ani*fps) // (len(positions)-1)
 
 
         # start with frame 0
@@ -529,15 +476,14 @@ def main():
     bpy.context.scene.render.image_settings.file_format=ani_setting["file_format"]
     output_dir = ani_setting["output_dir"]
 
-    for frame in tqdm(range(len_of_ani*24)):
+    for frame in tqdm(range(len_of_ani*fps)):
         bpy.context.scene.frame_set(frame)
         bpy.context.scene.render.filepath = output_dir + str(frame)
         bpy.ops.render.render('INVOKE_DEFAULT', write_still=True)
 
 
-    #bpy.ops.render.render('INVOKE_DEFAULT', animation=True, use_viewport = True, write_still=True)
-    #print("FFFF")
-    for frame in range(len_of_ani*24):
+
+    for frame in range(len_of_ani*fps):
         print(frame)
         get_location(sat[0], frame)
     #bpy.ops.render.view_show()
